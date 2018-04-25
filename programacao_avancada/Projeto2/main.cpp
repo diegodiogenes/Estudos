e6963126568e04a7dd1e21987207b6bfae3996f8
diego.diogenes <diego.diogenes@lais.huol.ufrn.br>
2018-04-24 22:54:31 -0300

    Finalizando main #1

--- programacao_avancada/Projeto2/main.cpp
+++ programacao_avancada/Projeto2/main.cpp
@@ -7,60 +7,32 @@ using namespace std;
 
 int main()
 {
-    Point p1, p2, p;
-    Poligono pol;
+    //instanciando novos objetos
     Point trans;
-    trans.setXY(0,0);
 
+    //criando novo objeto de retangulo
     Retangulo ret(0,0,3,4);
 
+    //imprimindo o retangulo
+    cout << "Retangulo" << endl;
+    ret.imprime();
+    //Área do retangulo
+    printf("\nÁrea do retângulo : %.1f \n", ret.area());
+
+    cout << "\nRetângulo transladado :" << endl;
+    ret.translada(-3,4);
     ret.imprime();
 
-    int quantidade;
+    printf("\nÁrea do retângulo transladado : %.1f \n", ret.area());
 
-    //Subtração entre pontos
-    p1.setXY(-5,2);
-    cout << "p1:" << endl;
-    p1.imprime();
+    trans.setXY(-3.0/2.0,2.0);
 
-    p.setXY(3,4);
-    cout << "p:" << endl;
-    p.imprime();
+    ret.rotaciona(30,trans);
 
-    p2 = p1.sub(p);
-    cout << "p2:" << endl;
-    p2.imprime();
+    cout << "\nRetangulo Rotacionado" << endl;
+    ret.imprime();
 
-    //Adição de vértices ao polígono
-    pol.adicionaVertice(0,0);
-    pol.adicionaVertice(2,0);
-    pol.adicionaVertice(0,2);
-    pol.adicionaVertice(2,2);
-
-    //Quantidade de vértices do polígono
-    quantidade = pol.quantidadeVertice();
-    cout << quantidade << endl;
-
-    //Área do polígono
-    cout << pol.area() << endl;
-
-    //Transladar o polígono (INCOMPLETO. O 'FOR' DA FUNÇÃO TRANSLADA NÃO ESTÁ FUNCIONANDO. DEVE SER UM ERRO DE UTILIZAÇÃO DO PTR)
-    pol.imprime();
-
-   // pol.translada(20,40);
-    cout << "Poligono Transladado" << endl;
-    pol.imprime();
-
-    cout << "Area do Poligono Transladado" << endl;
-    cout << pol.area() << endl;
-
-    pol.rotaciona(180, trans);
-
-    cout << "Poligono Rotacionado" << endl;
-    pol.imprime();
-
-    cout << "Area do poligono rotacionado" << endl;
-    cout << pol.area() << endl;
+    printf("\nÁrea do retângulo rotacionado : %.1f \n", ret.area());
 
     return 0;
 }
