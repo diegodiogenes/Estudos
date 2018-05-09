6b0851ed8a175e51c5ef1238bab6a58ab53c249d
Dorgival Rocha <dorgivalfilho10@outlook.com>
2018-05-09 08:36:23 -0300

    Implementação da header da classe Screen.

--- programacao_avancada/Projeto2/screen.cpp
+++ programacao_avancada/Projeto2/screen.cpp
@@ -2,5 +2,33 @@
 
 Screen::Screen(int nlin, int ncol)
 {
+    if(nlin>=0 && ncol>=0){
+        nlin = _nlin;
+        ncol = _ncol;
+    }
+    else
+        throw "Nº de linhas ou colunas menores que zero.";
+    matriz = vector<vector<char>>(_nlin, vector<char>(_ncol, ' '));
+
+
+}
+
+void Screen::setPixel(int x, int y)
+{
+    matriz[x][y] = brush;
+}
+
+void Screen::clear()
+{
+
+}
+
+void Screen::setBrush(char brush)
+{
+
+}
+
+ostream &Screen::operator<<(ostream &os, Screen &t)
+{
 
 }
--- programacao_avancada/Projeto2/screen.h
+++ programacao_avancada/Projeto2/screen.h
@@ -7,23 +7,65 @@
 
 using namespace std;
 
+/**
+ * @brief The Screen class
+ * @details
+ */
 class Screen
 {
 private:
+    /**
+     * @brief nlin
+     * @brief ncol
+     */
     int nlin, ncol;
 
+    /**
+     * @brief brush
+     */
     char brush;
+    /**
+     * @brief matriz
+     */
+    vector<vector<char>> matriz;
 
-    vector<vector<int>> matriz;
 public:
+    /**
+     * @brief Screen
+     * @details
+     * @param nlin
+     * @param ncol
+     */
     Screen(int nlin, int ncol);
 
+    /**
+     * @brief setPixel
+     * @details
+     * @param x
+     * @param y
+     */
     void setPixel(int x, int y);
 
+    /**
+     * @brief clear
+     * @details
+     */
     void clear();
 
+    /**
+     * @brief setBrush
+     * @details
+     * @param brush
+     */
     void setBrush(char brush);
 
+    /**
+     * @brief operator <<
+     * details
+     * @param os
+     * @param t
+     * @return
+     */
     friend ostream& operator<<(ostream &os, Screen &t);
 };
 
